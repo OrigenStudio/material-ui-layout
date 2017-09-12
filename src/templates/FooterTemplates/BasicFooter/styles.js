97be86400dbd235d44ca45bac387809c2feb6df4
PolGuixe <pol@zetoff.com>
2017-09-12 21:00:08 +0200

    footer templates

---
+++ src/templates/FooterTemplates/BasicFooter/styles.js
@@ -0,0 +1,14 @@
+const styles = () => ({
+  message: {
+    textAlign: 'center',
+    marginBottom: '30px',
+  },
+  logoImage: {
+    maxHeight: '50px',
+    maxWidth: '300px',
+    margin: '0',
+    padding: '0',
+  },
+});
+
+export default styles;
