a5c4f99b6c9de022b4937ccc24168cf74ff4137a
PolGuixe <pol@zetoff.com>
2017-09-13 15:29:37 +0200

    added the exports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-export default {};
+import Layout from './components/Layout';
+
+export { default as AppBar } from './components/NavBar';
+export { default as Footer } from './components/Footer';
+
+// I am changing the name from NavBar to AppBar
+export { default as BasicAppBar } from './templates/NavBar/BasicNavBar';
+export { default as BasicFooter } from './templates/Footer/BasicFooter';
+export { default as BasicDrawer } from './templates/Drawer/BasicDrawer';
+
+export default Layout;
