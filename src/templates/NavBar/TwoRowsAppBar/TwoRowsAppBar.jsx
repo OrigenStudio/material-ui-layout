9bbe5c4bb584e74684d7675933e782ed0415ffcf
PolGuixe <pol@zetoff.com>
2017-09-21 19:46:49 +0200

    fixed propTypes

--- src/templates/NavBar/TwoRowsAppBar/TwoRowsAppBar.jsx
+++ src/templates/NavBar/TwoRowsAppBar/TwoRowsAppBar.jsx
@@ -9,12 +9,12 @@ import styles from './styles';
 class TwoRowsAppBar extends PureComponent {
   static propTypes = {
     classes: PropTypes.shape({}),
-    topLeftContent: PropTypes.oneOf(PropTypes.element, PropTypes.arrayOf([PropTypes.element])),
-    topCenterContent: PropTypes.oneOf(PropTypes.element, PropTypes.arrayOf([PropTypes.element])),
-    topRightContent: PropTypes.oneOf(PropTypes.element, PropTypes.arrayOf([PropTypes.element])),
-    bottomLeftContent: PropTypes.oneOf(PropTypes.element, PropTypes.arrayOf([PropTypes.element])),
-    bottomCenterContent: PropTypes.oneOf(PropTypes.element, PropTypes.arrayOf([PropTypes.element])),
-    bottomRightContent: PropTypes.oneOf(PropTypes.element, PropTypes.arrayOf([PropTypes.element])),
+    topLeftContent: PropTypes.oneOf([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]),
+    topCenterContent: PropTypes.oneOf([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]),
+    topRightContent: PropTypes.oneOf([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]),
+    bottomLeftContent: PropTypes.oneOf([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]),
+    bottomCenterContent: PropTypes.oneOf([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]),
+    bottomRightContent: PropTypes.oneOf([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]),
   };
 
   static defaultProps = {
