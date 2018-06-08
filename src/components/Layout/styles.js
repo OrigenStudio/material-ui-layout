e02322039a05fe1ff17f5a3f52597b921fa92c85
PolGuixe <pol.guixe@gmail.com>
2018-06-08 16:36:53 +0200

    better code style

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -61,24 +61,26 @@ const styles = theme => ({
   },
   [theme.breakpoints.up('sm')]: {
     mainFixedAppBar: {
-      marginTop: `${theme.mixins.toolbar[theme.breakpoints.up('sm')]
-        .minHeight}px`,
+      marginTop: `${
+        theme.mixins.toolbar[theme.breakpoints.up('sm')].minHeight
+      }px`,
     },
     mainFixedTwoRowAppBar: {
       marginTop: `${theme.mixins.toolbar[theme.breakpoints.up('sm')].minHeight *
         2}px`,
     },
     mainGrow: {
-      height: `calc(100vh - ${theme.mixins.toolbar[theme.breakpoints.up('sm')]
-        .minHeight}px)`,
+      height: `calc(100vh - ${
+        theme.mixins.toolbar[theme.breakpoints.up('sm')].minHeight
+      }px)`,
     },
     mainGrowTwoRowAppBar: {
       height: `calc(100vh - ${theme.mixins.toolbar[theme.breakpoints.up('sm')]
         .minHeight * 2}px)`,
     },
     drawerHeaderTwoRowAppBar: {
-      minHeight: `${theme.mixins.toolbar[theme.breakpoints.up('sm')]
-        .minHeight * 2}px`,
+      minHeight: `${theme.mixins.toolbar[theme.breakpoints.up('sm')].minHeight *
+        2}px`,
     },
   },
   mainStickyFooter: {
