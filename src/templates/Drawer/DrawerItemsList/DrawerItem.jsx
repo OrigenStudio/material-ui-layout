f6c4be45ec1bcce82719a0c8e310cc7b67ab26b2
PolGuixe <pol.guixe@gmail.com>
2018-06-20 19:49:02 +0200

    improved typing

--- src/templates/Drawer/DrawerItemsList/DrawerItem.jsx
+++ src/templates/Drawer/DrawerItemsList/DrawerItem.jsx
@@ -1,23 +1,33 @@
 // @flow
 
-import React, { PureComponent } from 'react';
+import * as React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Icon from '@material-ui/core/Icon';
 
+type Item = {
+  iconName?: string,
+  icon?: any, // Not the best solution
+  onClick?: Function,
+  label: string,
+  href?: string,
+};
+
 type Props = {
-  item: Object,
+  item: Item,
   closeDrawer: Function,
 };
 
-class DrawerItem extends PureComponent<Props> {
+class DrawerItem extends React.PureComponent<Props> {
   handleClick = () => {
-    this.props.item.onClick();
-    this.props.closeDrawer();
+    if (this.props.item.onClick) {
+      this.props.item.onClick();
+      this.props.closeDrawer();
+    }
   };
 
-  renderIcon = (item: Object) => {
+  renderIcon = (item: Item) => {
     if (item.icon) {
       return <item.icon />;
     } else if (item.iconName) {
@@ -32,7 +42,7 @@ class DrawerItem extends PureComponent<Props> {
       <ListItem
         button
         onClick={item.onClick ? this.handleClick : null}
-        href={item.href || null}
+        href={item.href || undefined}
         component={item.href ? 'a' : undefined}
       >
         <ListItemIcon>{this.renderIcon(item)}</ListItemIcon>
