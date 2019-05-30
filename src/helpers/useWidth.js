71324ccd6398bdd5b764763981fad626c63fbe88
PolGuixe <pol.guixe@gmail.com>
2019-05-30 12:12:21 +0200

    added disclaimer

--- src/helpers/useWidth.js
+++ src/helpers/useWidth.js
@@ -10,6 +10,9 @@ const useWidth = (defaultWidth?: Breakpoint = 'xs') => {
   return (
     breakpoints.reverse().reduce<?Breakpoint>((output, key) => {
       const mediaQuery = theme.breakpoints.only(key);
+      // DISCLAIMER: the following hook is safe to use inside this loop
+      // because the breakpoints list will/should not change between calls
+      // eslint-disable-next-line react-hooks/rules-of-hooks
       const matches: boolean = useMediaQuery(mediaQuery);
 
       return !output && matches ? key : output;
